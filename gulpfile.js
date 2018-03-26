dc70fb367665e1736ab4ff5aee10f73dff343115
LAPTOP-JHLSCDA7\steve <steve@soaconsultingservices.com>
2018-03-26 09:53:24 -0400

    updated gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,31 @@ var clean = require('gulp-clean');
 var runSequence = require('gulp-run-sequence');
 var exec = require('child_process').exec;
 var requireDir = require('require-dir');
-//var mail = require('gulp-mail');
+var mail = require('gulp-mail');
 var gutil = require('gulp-util');
 var ftp = require('vinyl-ftp');
 var mailing = require('gulp-mailing');
 requireDir('./gulp/tasks', {recurse: true});
+
+// Help Commands
+
+gulp.task('ionic_help', function (cb) {
+  exec('ionic --help', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('cordova_help', function (cb) {
+  exec('cordova --help', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+// Platform Informational Commands
 
 gulp.task('ionic_cordova_platforms_ls', function (cb) {
   exec('ionic cordova platforms ls', function (err, stdout, stderr) {
@@ -15,70 +35,8 @@ gulp.task('ionic_cordova_platforms_ls', function (cb) {
     console.log(stderr);
     cb(err);
   });
-})
+});
 
-gulp.task('ionic_cordova_build_browser', function (cb) {
-  exec('ionic cordova build browser', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-})
-gulp.task('ionic_cordova_run_browser', function (cb) {
-  exec('ionic cordova run browser', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-})
-gulp.task('ionic_cordova_build_andriod', function (cb) {
-  exec('ionic cordova build android', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-})
-gulp.task('ionic_cordova_build_andriod_release', function (cb) {
-  exec('ionic cordova build android --release', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-})
-gulp.task('distribute', function (cb) {
-  exec('aws s3 cp platforms\\android\\app\\build\\outputs\\apk\\release\\app-release.apk s3://aws-website-angularorange-wcrwx/ ', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-})
-gulp.task('distribute_from_toplevel', function (cb) {
-  exec('aws s3 cp app-debug.apk s3://aws-website-angularorange-wcrwx/ ', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-})
-gulp.task('deploy', function (cb) {
-  runSequence('ionic_cordova_build_andriod_release', 'distribute', cb);
-});
-gulp.task('emulate', function (cb) {
-  runSequence('ionic_cordova_build_andriod', 'ionic_cordova_emulate_android', cb);
-});
-gulp.task('ionic_cordova_build_android', function (cb) {
-  exec('ionic cordova build android', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-})
-gulp.task('ionic_cordova_emulate_android', function (cb) {
-  exec('ionic cordova emulate android', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-})
 gulp.task('ionic_cordova_requirements', function (cb) {
   exec('ionic cordova requirements', function (err, stdout, stderr) {
     console.log(stdout);
@@ -86,65 +44,141 @@ gulp.task('ionic_cordova_requirements', function (cb) {
     cb(err);
   });
 })
-gulp.task('ionic_cordova_platform_add_ios', function (cb) {
-  exec('ionic cordova platform add ios', function (err, stdout, stderr) {
+
+// Distribute Commands
+
+gulp.task('distribute_android_release_to_aws', function (cb) {
+  exec('aws s3 cp platforms\\android\\app\\build\\outputs\\apk\\release\\app-release.apk s3://aws-website-angularorange-wcrwx/ ', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('distribute_android_debug_to_aws', function (cb) {
+  exec('aws s3 cp platforms\\android\\app\\build\\outputs\\apk\\debug\\app-debug.apk s3://aws-website-angularorange-wcrwx/ ', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
 })
+
+// Platform Commands
+
+// Platform Commands (Browser)
+
 gulp.task('ionic_cordova_platform_add_browser', function (cb) {
   exec('ionic cordova platform add browser', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
-})
+});
+
 gulp.task('ionic_cordova_platform_rm_browser', function (cb) {
   exec('ionic cordova platform rm browser', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
-})
+});
+
 gulp.task('ionic_cordova_build_browser', function (cb) {
   exec('ionic cordova build browser', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
-})
+});
+
 gulp.task('ionic_cordova_run_browser', function (cb) {
   exec('ionic cordova run browser', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
-})
+});
+
+// Platform Commands (Android)
+
+gulp.task('ionic_cordova_platform_add_androidd', function (cb) {
+  exec('ionic cordova platform add android', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('ionic_cordova_platform_rm_androidd', function (cb) {
+  exec('ionic cordova platform rm android', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('ionic_cordova_build_andriod', function (cb) {
+  exec('ionic cordova build android', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('ionic_cordova_build_andriod_release', function (cb) {
+  exec('ionic cordova build android --release', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('ionic_cordova_emulate_android', function (cb) {
+  exec('ionic cordova emulate android', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+
+gulp.task('deploy_android', function (cb) {
+  runSequence('ionic_cordova_build_andriod_release', 'distribute_android_release_to_aws', cb);
+});
+
+// Platform Commands (iOS)
+
+gulp.task('ionic_cordova_platform_add_ios', function (cb) {
+  exec('ionic cordova platform add ios', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
 gulp.task('ionic_cordova_platform_rm_ios', function (cb) {
   exec('ionic platform rm ios', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
-})
-gulp.task('ionic_help', function (cb) {
-  exec('ionic --help', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-})
+});
+
+// Run Commands
+
 gulp.task('ionic_serve', function (cb) {
   exec('ionic serve', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
-})
-gulp.task('ionic:watch:before', ['watch'])
+});
 
-var smtpInfo = {
+gulp.task('ionic:watch:before', ['watch']);
+
+// Email Notifications using AWS Email Server
+
+var smtpInfoAws = {
   auth: {
     user: "AKIAJCGHN4IHSWATXMCA",
     pass: "AjAiFzGqtjC9L4JwZ3Vr2REEfd20lsXuxvAKbTrAyB/p"
@@ -162,10 +196,11 @@ gulp.task('email_distribution', function () {
         'steve@soaconsultingservices.com'
       ],
       from: 'steve@soaconsultingservices.com',
-      smtp: smtpInfo
+      smtp: smtpInfoAws
     }));
 });
 
+// Email Notifications using Mochahost Email Server
 
 var smtpInfoMochahost = {
   auth: {
@@ -176,6 +211,7 @@ var smtpInfoMochahost = {
   secureConnection: true,
   port: 2525
 };
+
 gulp.task('email_attachment', function () {
   return gulp.src('./mymailmessage.html')
     .pipe(mailing({
@@ -188,7 +224,7 @@ gulp.task('email_attachment', function () {
         {
           path: 'app-release.apk'
         }
-        ],
-    smtp: smtpInfo
+      ],
+      smtp: smtpInfoMochahost
     }));
 });
